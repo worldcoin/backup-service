2b4593293af22d2ee6802e986f34a143975b46d9
sevazhidkov <zhidkovseva@gmail.com>
2025-04-11 21:07:52 -0700

    feat: endpoint for a passkey challenge to retrieve backup

---
+++ tests/retrieve_challenge_integration.rs
@@ -0,0 +1,77 @@
+mod common;
+
+use crate::common::send_post_request;
+use axum::http::StatusCode;
+use http_body_util::BodyExt;
+use serde_json::json;
+
+#[tokio::test]
+async fn test_retrieve_challenge() {
+    let response = send_post_request("/retrieve/challenge/passkey", json!({})).await;
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body = response.into_body().collect().await.unwrap().to_bytes();
+    let response: serde_json::Value = serde_json::from_slice(&body).unwrap();
+
+    dbg!(&response);
+
+    assert_eq!(
+        response["challenge"]["publicKey"]["rp"]["name"].as_str(),
+        Some("World App")
+    );
+    assert_eq!(
+        response["challenge"]["publicKey"]["rp"]["id"].as_str(),
+        Some("keys.world.org")
+    );
+    assert_eq!(
+        response["challenge"]["publicKey"]["challenge"]
+            .as_str()
+            .unwrap()
+            .len(),
+        43
+    );
+    assert_eq!(
+        response["challenge"]["publicKey"]["pubKeyCredParams"]
+            .as_array()
+            .unwrap()
+            .len(),
+        2
+    );
+    assert_eq!(
+        response["challenge"]["publicKey"]["timeout"].as_u64(),
+        Some(300000)
+    );
+    assert_eq!(
+        response["challenge"]["publicKey"]["authenticatorSelection"]["residentKey"].as_str(),
+        Some("discouraged")
+    );
+    assert_eq!(
+        response["challenge"]["publicKey"]["authenticatorSelection"]["requireResidentKey"]
+            .as_bool(),
+        Some(false)
+    );
+    assert_eq!(
+        response["challenge"]["publicKey"]["authenticatorSelection"]["userVerification"].as_str(),
+        Some("required")
+    );
+    assert_eq!(
+        response["challenge"]["publicKey"]["attestation"].as_str(),
+        Some("none")
+    );
+    assert_eq!(
+        response["challenge"]["publicKey"]["extensions"]["credentialProtectionPolicy"].as_str(),
+        Some("userVerificationRequired")
+    );
+    assert_eq!(
+        response["challenge"]["publicKey"]["extensions"]["enforceCredentialProtectionPolicy"]
+            .as_bool(),
+        Some(false)
+    );
+    assert_eq!(
+        response["challenge"]["publicKey"]["extensions"]["uvm"].as_bool(),
+        Some(true)
+    );
+
+    assert!(response["token"].as_str().is_some());
+}
