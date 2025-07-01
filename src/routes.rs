26c503074e1a21a5135e44179149fbe68bdc9e4e
aurel-fr <105201452+aurel-fr@users.noreply.github.com>
2025-07-01 15:56:01 -0700

    docs

--- src/routes.rs
+++ src/routes.rs
@@ -8,6 +8,7 @@ use crate::{
     attestation_gateway::AttestationGateway,
     routes::add_sync_factor_challenge_keypair::AddSyncFactorChallengeKeypairRequest,
 };
+use aide::axum::routing::post_with;
 use aide::axum::{
     routing::{get, post},
     ApiRouter,
@@ -66,8 +67,10 @@ pub fn handler(environment: Environment) -> ApiRouter {
         )
         .api_route(
             "/retrieve/from-challenge",
-            post(retrieve_from_challenge::handler)
-                .route_layer(middleware::from_fn(AttestationGateway::validator)),
+            post_with(retrieve_from_challenge::handler, |op| {
+                op.security_requirement("AttestationToken")
+            })
+            .route_layer(middleware::from_fn(AttestationGateway::validator)),
         )
         // Add new factor for future sync after recovery
         .api_route(
