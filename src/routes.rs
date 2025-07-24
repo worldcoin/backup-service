865668d819de193baab906cccb300e0ba1c77d2d
pd <paolodamico@users.noreply.github.com>
2025-07-24 11:48:08 +0700

    post-merge fix

--- src/routes.rs
+++ src/routes.rs
@@ -120,5 +120,6 @@ pub fn handler(environment: Environment) -> ApiRouter {
     ApiRouter::new()
         .merge(docs::handler())
         .api_route("/health", get(health::handler))
+        .api_route("/ready", post(ready::handler))
         .nest("/v1", v1_routes)
 }
