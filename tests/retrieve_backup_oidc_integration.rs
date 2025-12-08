c8221dee10c91e512aff3e2a1d356e970ed4d59e
pd <paolodamico@users.noreply.github.com>
2025-12-08 13:05:36 -0800

    feat: improve errors for failed signature

--- tests/retrieve_backup_oidc_integration.rs
+++ tests/retrieve_backup_oidc_integration.rs
@@ -251,7 +251,7 @@ async fn test_retrieve_backup_with_different_keypair() {
             "allowRetry": false,
             "error": {
                 "code": "signature_verification_error",
-                "message": "Signature verification failed.",
+                "message": "Signature verification failed",
             }
         })
     );
